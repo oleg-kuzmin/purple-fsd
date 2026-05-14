674104b62c2df25ad163d9c38d26d6c4e8e2eb02
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-14 12:16:24 +0300

    add shared

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,11 @@
+export { Button } from './Button/Button';
+export { Card } from './Card/Card';
+export { Checkbox } from './Checkbox/Checkbox';
+export { Graph } from './Graph/Graph';
+export { InfoBox } from './InfoBox/InfoBox';
+export { Input } from './Input/Input';
+export { Link } from './Link/Link';
+export { RadioButton } from './RadioButton/RadioButton';
+export { Rating } from './Rating/Rating';
+export { Tabs } from './Tabs/Tabs';
+export { Tag } from './Tag/Tag';
